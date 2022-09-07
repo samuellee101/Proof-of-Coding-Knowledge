b24aeb52b842a2723194acb21307c152bf361fc9
samuellee101 <69314420+samuellee101@users.noreply.github.com>
2022-09-06 19:21:25 -0500

    Add files via upload

---
+++ C++/priorityQueueLL.h
@@ -0,0 +1,92 @@
+#pragma once
+
+template <class T>
+
+class priorityQueueLL
+{
+private:
+    class node
+    {
+    public:
+        node* p;
+        node* n;
+        T dtype;
+        node(T x)
+        {
+            p = nullptr;
+            n = nullptr;
+            dtype = x;
+        }
+
+    };
+
+    node* h;
+public:
+
+    priorityQueueLL()
+    {
+        h = nullptr;
+    }
+
+    ~priorityQueueLL()
+    {
+        while (h != nullptr)
+        {
+            node* temp = h;
+            h = h->n;
+            h->p = nullptr;
+            delete temp;
+        }
+    }
+
+    //delete the minimum
+    T extractMin()
+    {
+        node* f = h;
+        h = h->n;
+        T dtype = f->dtype;
+        delete f;
+        return dtype;
+    }
+
+    bool empty()
+    {
+        if (h != nullptr)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    void insert(T x)
+    {
+        node* n = new node(x);
+
+        if (h == nullptr) {
+            h = n;
+            return;
+        }
+
+        if (x < h->dtype) {
+            n->n = h;
+            h = n;
+            return;
+        }
+
+        node* p = h, * curr = h->n;
+
+        while (curr != nullptr) {
+
+            if (x < curr->dtype) {
+                break;
+            }
+
+            p = curr;
+            curr = curr->n;
+        }
+
+        p->n = n;
+        n->n = curr;
+    }
+
+};
---
+++ C++/queueLL.h
@@ -0,0 +1,115 @@
+#pragma once
+
+template <class T>
+
+class queueLL
+{
+private:
+	class node
+	{
+	public:
+		node* n;
+		//node* p;
+		T data;
+    
+
+		node(T x)
+		{
+			data = x;
+			n = nullptr;
+			//p = nullptr;
+		}
+	};
+
+	node* front;
+	node* back;
+  int size;
+
+public:
+	queueLL()
+	{
+		front = nullptr;
+		back = nullptr;
+	}
+
+	~queueLL()
+	{
+		node* garbage;
+		while (front != nullptr)
+		{
+			garbage = front;
+			front = front->n;
+			delete garbage;
+		}
+    size = 0;
+	}
+
+	void decimate()
+	{
+		node* one = front;
+		node* two = nullptr;
+		int temp = 1;
+
+
+		while (one != nullptr)
+		{
+			if (temp==9)
+			{
+				if (one->n != nullptr)
+				{
+					two = one->n;
+					two->n = one->n;
+					//delete two->data;
+					delete two;
+					temp = -1;
+				}
+				else{
+          break;
+        }
+			}
+			else
+      {
+        one = one->n;
+      }
+			temp++;
+		}
+  }
+
+	void enqueue(T x)
+	{
+		node* added;
+		added = new node(x);
+		if (front == nullptr)
+		{
+			front = added;
+			back = added;
+		}
+		else
+		{
+			back->n = added;
+			back = back->n;
+		}
+    size++;
+	}
+
+	T dequeue()
+	{
+		node* goaway;
+		goaway = front;
+		front = front->n;
+		T a = goaway->data;
+		delete[] goaway;
+    size--;
+		return a;
+	}
+
+	bool empty() 
+	{
+		if (front == nullptr)
+		{
+			return true;
+		}
+		return false;
+	}
+
+};
