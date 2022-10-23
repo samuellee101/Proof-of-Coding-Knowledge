3982034fdafc8637d4fd9ea034810c9830e1bd41
samuellee101 <samuellee010@yahoo.com>
2022-10-23 08:02:55 -0500

    modified:   lib/utils/coffee_tools.dart 	modified:   test/coffee_tools_test.dart

--- lib/utils/coffee_tools.dart
+++ lib/utils/coffee_tools.dart
@@ -1,4 +1,14 @@
 class CoffeeTools {
+  String _device;
+
+  CoffeeTools(String device) {
+    _device = device;
+  }
+
+  String device() {
+    return _device;
+  }
+
   // Input: number of cups (positive integer)
   // Output: number of ounces in a cup
   static int cupsToOunces(int cups) {
@@ -6,5 +16,26 @@ class CoffeeTools {
       throw ArgumentError();
     }
     return cups * 6;
+  }
+
+  static double ratioDrip(int cups) {
+    int oWater = cupsToOunces(cups);
+    double gWater = ouncesToGrams(oWater);
+    double gCoffee = double.parse((gWater / 17).toStringAsFixed(2));
+    return gCoffee;
+  }
+
+  static double ouncesToGrams(int ounces) {
+    if (ounces <= 0) {
+      throw ArgumentError();
+    }
+    return double.parse(((ounces / 6) * 177.42).toStringAsFixed(2));
+  }
+
+  static double ratioFrenchPress(int cups) {
+    int oWater = cupsToOunces(cups);
+    double gWater = ouncesToGrams(oWater);
+    double gCoffee = double.parse((gWater / 14).toStringAsFixed(2));
+    return gCoffee;
   }
 }
--- test/coffee_tools_test.dart
+++ test/coffee_tools_test.dart
@@ -18,7 +18,57 @@ void main() {
     });
 
     test('throws ArgumentError on negative number', () {
-      expect(() => CoffeeTools.cupsToOunces(0), throwsArgumentError);
+      expect(() => CoffeeTools.cupsToOunces(-4), throwsArgumentError);
+    });
+  });
+
+  group("ratioFrenchPress", () {
+    test('calculates coffee to water ratio for French Press', () {
+      var ratio = CoffeeTools.ratioFrenchPress(5);
+      expect(ratio, 63.36);
+    });
+
+    test('throws ArgumentError on zero', () {
+      expect(() => CoffeeTools.ratioFrenchPress(0), throwsArgumentError);
+    });
+
+    test('throws ArgumentError on negative number', () {
+      expect(() => CoffeeTools.ratioFrenchPress(-2), throwsArgumentError);
+    });
+  });
+
+  group("ouncesToGrams", () {
+    test('calculates grams on 6 ounces', () {
+      var grams = CoffeeTools.ouncesToGrams(6);
+      expect(grams, 177.42);
+    });
+
+    test('calculates grams on positive number of ounces', () {
+      var grams = CoffeeTools.ouncesToGrams(30);
+      expect(grams, 887.1);
+    });
+
+    test('throws ArgumentError on zero', () {
+      expect(() => CoffeeTools.ouncesToGrams(0), throwsArgumentError);
+    });
+
+    test('throws ArgumentError on negative number', () {
+      expect(() => CoffeeTools.ouncesToGrams(-4), throwsArgumentError);
+    });
+  });
+
+  group("ratioDrip", () {
+    test('calculates coffee to water ratio for Drip Machine', () {
+      var ratio = CoffeeTools.ratioDrip(5);
+      expect(ratio, 52.18);
+    });
+
+    test('throws ArgumentError on zero', () {
+      expect(() => CoffeeTools.ratioDrip(0), throwsArgumentError);
+    });
+
+    test('throws ArgumentError on negative number', () {
+      expect(() => CoffeeTools.ratioDrip(-4), throwsArgumentError);
     });
   });
 }
